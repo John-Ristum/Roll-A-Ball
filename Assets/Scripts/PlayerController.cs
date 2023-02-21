d296bb0d6ea1d41f4da980348d56f3c1f69dca9d
John-Ristum <125324914+John-Ristum@users.noreply.github.com>
2023-02-21 12:03:08 +1100

    Pickups-&-Timer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed = 1;
-    public float speed2;
+    int pickupCount;
+    Timer timer;
 
     void Start()
     {
         //Get the Rigidbidy component of the gameObject
         rb = GetComponent<Rigidbody>();
+        //Get the number of pickups in our scene
+        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        CheckPickups();
+        //Get the Yimer object
+        timer = FindObjectOfType<Timer>();
+        timer.StartTimer();
     }
 
     void Update()
@@ -26,5 +33,38 @@ public class PlayerController : MonoBehaviour
 
         //Add force to the rigidbody based on our movement vector
         rb.AddForce(movement * speed);
+
+
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If the other object contains the Pickup tag, destroy it
+        if(other.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            //Decriment the pickup count
+            pickupCount -= 1;
+            CheckPickups();
+        }
+    }
+
+    void CheckPickups()
+    {
+        Debug.Log(pickupCount + " more remaining");
+
+        if (pickupCount == 0)
+        {
+            WinGame();
+        }
+    }
+
+
+    void WinGame()
+    {
+        timer.StopTimer();
+        Debug.Log("YOU WIN! Time: " + timer.GetTime().ToString("F3"));
+    }
+
+
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float currentTime;
+    bool timing;
+
+    public void StartTimer()
+    {
+        timing = true;
+    }
+
+    public void StopTimer()
+    {
+        timing = false;
+    }
+
+    public float GetTime()
+    {
+        return currentTime;
+    }
+
+
+    void Update()
+    {
+        if(timing)
+        {
+            currentTime += Time.deltaTime;
+        }
+
+    }
+}
